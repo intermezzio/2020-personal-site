55f5d8fd98c978b2772cf7b11ceda8682dc34804
intermezzio <amascillaro@gmail.com>
2020-06-18 14:12:08 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
-git clone git@github.com:intermezzio/intermezzio.github.io.git
-cd intermezzio.github.io
-
 git worktree add -B master docs/ origin/master
 
 cd themes/
 rm -rf hugo-whisper-theme
 git rm --cached hugo-whisper-theme
-git submodule add git://github.com/intermezzio/hugo-whisper-theme.git
+git submodule add https://github.com/intermezzio/hugo-whisper-theme.git
 cd ..
